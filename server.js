50e941307504759b75c429322cf3f0e610f90460
rudisiona <rsmageo@gmail.com>
2025-06-20 18:06:53 -0700

    wip

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+const dotenv = require('dotenv')
+dotenv.config()
+
 const express = require("express")
-const https = require('https');
+
 const app = express()
 app.use(express.urlencoded({ extended: true}))
-app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
@@ -10,12 +13,12 @@ app.get('/', (req, res) => {
 })
 app.post('/weather', (req, res) => {
     const zip = req.body.zip
-    const apiKey = '27fedef8c659941e7abb8e570e950923'; // 🔑 Replace this with your actual API key
-    const url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=imperial`;
-  
-    console.log("Zip", zip);
-  
-  
+
+   const key = process.env.APIKEY
+   const url = 'https://api.openweathermap.org/data/2.5/weather?zip=${zip},${country}&appid=${key}&units=${units}'
+   console.log(req.body)
+  res.render("weather/show.ejs", {zip})
+
 })
 
 app.listen(3000, () => console.log("we are running on port 3000"))
